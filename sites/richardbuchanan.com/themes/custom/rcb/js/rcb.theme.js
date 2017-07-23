0cc9528c098270deca3818a6c671211f43096b3e
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-07-23 06:09:57 -0400

    Updated user profile templates and main layout behaviors

--- sites/richardbuchanan.com/themes/custom/rcb/js/rcb.theme.js
+++ sites/richardbuchanan.com/themes/custom/rcb/js/rcb.theme.js
@@ -12,6 +12,45 @@
       $('pre code').each(function(i, block) {
         hljs.highlightBlock(block);
       });
+
+      $(document).ready(function () {
+        setMainHeight()
+      });
+
+      $(window).resize(function() {
+        setMainHeight()
+      });
+    }
+  };
+
+  function getToolbarHeight() {
+    var $body = $('body');
+    var $toolbar = $('#toolbar-administration');
+    var $navbarVisible = $('.uk-navbar-container:visible');
+    var $toolbarHeight = 0;
+
+    if ($toolbar.length) {
+      $toolbarHeight = 40;
+    }
+    if ($body.hasClass('toolbar-horizontal') && $body.hasClass('toolbar-tray-open')) {
+      $toolbarHeight = 79;
+    }
+    if ($navbarVisible) {
+      var $navbar = $('.uk-navbar-container');
+      $toolbarHeight += ($navbar.height() + 2)
+    }
+
+    return $toolbarHeight
+  }
+
+  function setMainHeight() {
+    var $html = $('html');
+    var $htmlHeight = $html.height();
+    var $main = $('.rcb-main');
+    var $mainHeight = $main.height();
+
+    if ($mainHeight < $htmlHeight) {
+      $main.height($htmlHeight - getToolbarHeight())
     }
   }
 })(jQuery, Drupal);
