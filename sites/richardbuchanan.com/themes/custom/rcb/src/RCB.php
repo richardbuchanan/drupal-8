8c3486da04d0e83c6309d793a206098f8b73d40b
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-07-23 00:17:54 -0400

    Added templates and styling for book trees and navigations

--- sites/richardbuchanan.com/themes/custom/rcb/src/RCB.php
+++ sites/richardbuchanan.com/themes/custom/rcb/src/RCB.php
@@ -2,8 +2,35 @@
 
 namespace Drupal\rcb;
 
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
 /**
  * Provides helper functions for the RCB theme.
  */
 class RCB {
+
+  public static function systemPoweredByContent() {
+    // Create the urls.
+    $drupal_url = Url::fromUri('https://www.drupal.org');
+    $uikit_url = Url::fromUri('https://getuikit.com');
+    $url_options = array('attributes' => array('target' => '_blank'));
+    $drupal_url->setOptions($url_options);
+    $uikit_url->setOptions($url_options);
+
+    // Set link text.
+    $drupal_text = t('Drupal');
+    $uikit_text = t('UIkit');
+
+    // Create the links.
+    $drupal_link = Link::fromTextAndUrl($drupal_text, $drupal_url);
+    $uikit_link = Link::fromTextAndUrl($uikit_text, $uikit_url);
+
+    // Creat the markup.
+    $content = [
+      '#markup' => t('Powered by ') . $drupal_link->toString() . ' and ' . $uikit_link->toString(),
+    ];
+
+    return $content;
+  }
 }
