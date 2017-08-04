0482503120973f7e8422670731882c605bb9f1bd
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-08-04 11:53:46 -0400

    Added new theme

---
+++ sites/richardbuchanan.com/themes/custom/rcb_theme/js/rcb_theme.theme.js
@@ -0,0 +1,14 @@
+/**
+ * @file
+ * Attaches behaviors for rcb_theme.
+ */
+
+(function ($) {
+
+  'use strict';
+
+  Drupal.behaviors.RCBTheme = {
+    attach: function () {
+    }
+  };
+})(jQuery);
