d2ec89db450845e78723f84c4c5a18247438f1c9
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-08-06 20:48:45 -0400

    Updated contributed modules and themes

--- sites/richardbuchanan.com/themes/contrib/uikit/src/UIkit.php
+++ sites/richardbuchanan.com/themes/contrib/uikit/src/UIkit.php
@@ -19,7 +19,7 @@ class UIkit {
    *
    * @var string
    */
-  const UIKIT_LIBRARY_VERSION = '3.0.0-beta.25';
+  const UIKIT_LIBRARY_VERSION = '3.0.0-beta.27';
 
   /**
    * The Drupal project page for the UIkit base theme.
